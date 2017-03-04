b04bade8b9b7b378e047731a09b8080092fb6adc
Niklas1941 <Niklas1941@hotmail.com>
2017-03-04 21:10:21 +0200

    some minor changes

--- js.js
+++ js.js
@@ -1,18 +1,22 @@
 function Read(){
 	var v = document.getElementById('input1');
 	var o = document.getElementById('output');
+	var mv = v.value;
 
 	//Check for slash
-	var slash = v.value.search("/");
+	var slash = mv.search("/");
 
 	if (slash == 0){
 		//Init the key words
-		var array1 = ["shout", "clear", "help"];
+		var array1 = ["shout", "clear", "help"];  //Keep these two arrays same length
+		var array2 = ["You can shout things","Clear the output","Show this \"Window\""]; //It's easer to do it this way, but maeby i will combine them... maybe...
+		mv = mv.toLowerCase();
+		//console.log(mv);
 
 		//Search for the key words
 		for (var i = 0; i < array1.length; i++) {
 
-			var KeyWords = v.value.search(array1[i]);
+			var KeyWords = mv.search(array1[i]);
 
 			//If found then return it to previous if statement 
 			if (KeyWords != -1){
@@ -27,10 +31,10 @@ function Read(){
 
 			case "shout":
 
-				v.value += " ";
-				v = v.value.slice(7, -1);
-				v = v.toUpperCase();
-				o.value += v + "!\r\n";
+				mv += " ";
+				mv = mv.slice(7, -1);
+				mv = mv.toUpperCase();
+				o.value += mv + "!\r\n";
 
 				break;
 
@@ -42,8 +46,9 @@ function Read(){
 
 			case "help":
 
-				o.value += "This is just a test!" + "\r\n";
-
+				for (var i = 0; i < array1.length; i++) {
+					o.value += "/" + array1[i] + " - " + array2[i] + "\r\n"; 
+				}
 			break;
 
 			default:
